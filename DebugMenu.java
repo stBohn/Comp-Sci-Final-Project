5345dc2fee1c8dc39dbb578893fac64703cd499a
BevenStoner <sharjrwick@gmail.com>
2025-06-01 13:41:10 -0400

    Basically Finished

--- DebugMenu.java
+++ DebugMenu.java
@@ -24,8 +24,25 @@ public class DebugMenu
         JLabel background = new JLabel(backgroundIcon);
         
         JButton cookie = new JButton();
-        
-        cookie.setBounds(300,300,300,300);
+        cookie.setIcon(new ImageIcon(filePath+"Cookie.png"));
+        cookie.setBounds(250,150,300,300);
+        cookie.setRolloverIcon(new ImageIcon(filePath+"RollOverCookie.png"));
+        cookie.setContentAreaFilled(false);
+        cookie.setBorderPainted(false);
+        int[] timesClicked = {0};
+        cookie.addActionListener(e -> {
+            SwingUtilities.invokeLater(() -> {
+                if(timesClicked[0]<=0){
+                    SaveGame.saveToFile("Save Data\\Save.txt", ""+0+"a"+0+"b"+0+"c"+0+"d"+0+"e"+0+"f"+0+"g"+0+"h"+0+"i"+1+"j");
+                    timesClicked[0]++;
+                }
+                else{
+                    SaveGame.saveToFile("Save Data\\Save.txt", ""+1000+"a"+0+"b"+1+"c"+10+"d"+100+"e"+1000+"f"+0+"g"+0+"h"+1+"i"+4+"j");
+                    timesClicked[0]++;
+                }
+            });
+        });
+        cookie.setFocusPainted(false);
         
         ImageIcon backIcon = new ImageIcon(filePath+"BACK.png");
         ImageIcon backIconRollOver = new ImageIcon(filePath+"BACKRollOver.png");
@@ -52,6 +69,7 @@ public class DebugMenu
             });
         });
         
+        contentPanel.add(cookie);
         contentPanel.add(backButton);
         contentPanel.add(background);
         
--- HelpMenu.java
+++ HelpMenu.java
@@ -73,14 +73,8 @@ public class HelpMenu
         int[] timesClicked = {0};
         debugButton.addActionListener(e -> {
             SwingUtilities.invokeLater(() -> {
-                if(timesClicked[0]<=0){
-                    SaveGame.saveToFile("Save Data\\Save.txt", ""+0+"a"+0+"b"+0+"c"+0+"d"+0+"e"+0+"f"+0+"g"+0+"h"+0+"i"+1+"j");
-                    timesClicked[0]++;
-                }
-                else{
-                    SaveGame.saveToFile("Save Data\\Save.txt", ""+1000+"a"+0+"b"+1+"c"+10+"d"+100+"e"+1000+"f"+0+"g"+0+"h"+1+"i"+4+"j");
-                    timesClicked[0]++;
-                }
+                new DebugMenu();
+                frame.dispose();
             });
         });
         debugButton.setFocusPainted(false);
